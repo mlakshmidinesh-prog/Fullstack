51b86548a7833e0918a1cc36f4f87e6fcf406ad5
mlakshmidinesh-prog <mlakshmidinesh@gmail.com>
2026-04-16 21:52:58 +0530

    Add files via upload

---
+++ student_app/config.py
@@ -0,0 +1,5 @@
+class config:
+    SECRET_KEY='dev_secret_key'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:Dinesh%4018102005@localhost/student_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    
